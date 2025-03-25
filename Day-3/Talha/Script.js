8fe95342ca892afbe67cc00dad715466025f17bd
Talha-24 <talhabhatti82542@gmail.com>
2025-03-26 00:28:21 +0500

    Day 3 Completed

--- Day-3/Talha/Script.js
+++ Day-3/Talha/Script.js
@@ -1,14 +1,34 @@
 
+//Programme 1
 
+function countDigit(num) {
+    let total = 0;
+    let str = num.toString();
+    for (let n of str) {
+        total++;
+    }
 
-function countDigit(num){
-   let total=0;
-   let str= num.toString();
-   for(let n of str){
-     total++;
-   }
- 
-   return total;
-    
+    return total;
+
 }
 console.log(countDigit(18239232));
+
+//Programme 2
+
+
+function sumOfdigit(num) {
+
+    let number = num.toString();
+    let sum = 0;
+
+    for (let n of number) {
+        for (let i = 0; i < 10; i++) {
+            if (n == i) {
+                sum += i;
+            }
+        }
+    }
+
+    return sum;
+}
+sumOfdigit();
